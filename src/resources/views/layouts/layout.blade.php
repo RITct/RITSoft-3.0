9d48984876373875cb5b14545f20840d672de552
Sidharth <sidforjee@gmail.com>
2021-03-05 15:53:33 +0530

    updated views

---
+++ src/resources/views/layouts/layout.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ URL::asset("app.css") }}"/>
+        <title>RITSoft | @yield('title')</title>
+    </head>
+    <body>
+        @section('content')
+        @show
+    </body>
+</html>
