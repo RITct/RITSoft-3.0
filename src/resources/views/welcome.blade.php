68bf0166a67f7bf00322e31da9b945ca5ed0156e
Sidharth <sidharthajithkumar@gmail.com>
2021-06-22 01:25:05 +0530

    added faculty in homepage

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -4,4 +4,5 @@
     <a href="{{route("roles.index")}}">Roles</a><br/>
     <a href="{{route("users.index")}}">Users</a><br/>
     <a href="{{route('attendance.index')}}">Attendance</a>
+    <a href="{{route('faculty.index')}}">Faculty</a>
 @endsection
